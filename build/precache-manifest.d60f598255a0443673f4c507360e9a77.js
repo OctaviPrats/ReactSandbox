8fb7ec77d8c5b77a9cb0655ecfa749c8a4e428db
octaviprats <octavi.prats@est.fib.upc.edu>
2020-08-30 18:25:06 +0200

    upload build
    to github pages

---
+++ build/precache-manifest.d60f598255a0443673f4c507360e9a77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19bcb8697e920c2d9b0dad4050b19095",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4defeac1f2d35daf9d74",
+    "url": "/static/css/main.ae547d6c.chunk.css"
+  },
+  {
+    "revision": "08270483fbe44fb8f9f0",
+    "url": "/static/js/2.7f973b8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f973b8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4defeac1f2d35daf9d74",
+    "url": "/static/js/main.f8c4d0de.chunk.js"
+  },
+  {
+    "revision": "89ac118f8d664424e2aa",
+    "url": "/static/js/runtime-main.40fd9fda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
